9f4f925252f90d75482020e16530ce42785b8466
llammann <bd7n2v9tr@code.du.az>
2024-05-18 10:50:35 +0000

    The last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,6 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
+if number < 0:
+    last_digit = -1* last_digit
 print(f"Last digit of {number} is {last_digit} and is ",end="")
 
 if last_digit == 0:
