f93a27ecf55140f23234baf861fd96756fae6daa
llammann <bd7n2v9tr@code.edu.az>
2024-05-18 20:08:42 +0000

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+alphabet=''
+for alpha in range(97,123):
+    if(chr(alpha) == 'q' or chr(alpha) == 'e'):
+        continue
+    else:
+        alphabet += chr(alpha)
+print("{}".format(alphabet), end="")
