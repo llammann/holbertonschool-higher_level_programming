44c9438f18481c175d91323647f07b3308fd4d93
llammann <bd7n2v9tr@code.edu.az>
2024-05-19 20:05:20 +0000

    Who are you

--- python-import_modules/4-hidden_discovery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import hidden_4
-
-if _name_ == "_main_":
-    
-    module_names = sorted(name for name in dir(hidden_4) if not name.startswith('__'))
-
-    for name in module_names:
-        print(name)
