0eef58532c1e61808d6b51282014936566a85b22
llammann <bd7n2v9tr@code.edu.az>
2024-05-19 16:32:02 +0000

    My first toolbox!

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+def main():
+    a = 10
+    b = 5
+
+    results = [
+        "{} + {} = {}".format(a, b, add(a, b)),
+        "{} - {} = {}".format(a, b, sub(a, b)),
+        "{} * {} = {}".format(a, b, mul(a, b)),
+        "{} / {} = {}".format(a, b, div(a, b))
+    ]
+
+    for result in results:
+        print(result)
+
+if __name__ == "__main__":
+    main()
+
