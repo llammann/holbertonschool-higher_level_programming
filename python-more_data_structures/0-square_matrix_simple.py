eececa080356d32e2f494d7449b788f462079bae
llammann <bd7n2v9tr@code.edu.az>
2024-05-25 20:51:36 +0000

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -7,6 +7,5 @@ def square_matrix_simple(matrix=[]):
         new_row = []
         for value in row:
             new_row.append(value ** 2)
-            
         new_matrix.append(new_row)
     return new_matrix
