791ff2cc45d56251a7a8727b51b567faada4f197
llammann <bd7n2v9tr@code.edu.az>
2024-05-25 22:07:52 +0000

    Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,10 +4,8 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    
     max_score = float('-inf')
     best_key = None
-
     for key, value in a_dictionary.items():
         if value > max_score:
             max_score = value
