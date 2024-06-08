388bbce70ce19de883a16942a4ee9a5004f0a550
llammann <bd7n2v9tr@code.edu.az>
2024-06-08 18:30:12 +0000

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 
 import sys
 from os.path import exists
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 
 def add_items_to_list_and_save():
