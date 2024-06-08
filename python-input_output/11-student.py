d1d9c88f01d232a6739610bfe3d0362d5cc81662
llammann <bd7n2v9tr@code.edu.az>
2024-06-08 22:20:22 +0000

    Student to disk and reload

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,19 +1,11 @@
-#!/usr/bin/python3
-"""
-This module defines a class `Student`.
-"""
-
-
 class Student:
     """
-    Represents a student with
-    attributes first_name, last_name, and age.
+    Represents a student with attributes first_name, last_name, and age.
     """
 
     def __init__(self, first_name, last_name, age):
         """
-        Initializes a Student object with
-        first name, last name, and age.
+        Initializes a Student object with first name, last name, and age.
 
         Args:
             first_name (str): The first name of the student.
@@ -26,33 +18,27 @@ class Student:
 
     def to_json(self, attrs=None):
         """
-        Retrieves a dictionary representation of
-        the student instance.
+        Retrieves a dictionary representation of the student instance.
 
         Args:
             attrs (list): List of attribute names to retrieve.
-            If None, retrieve all attributes.
+                          If None, retrieve all attributes.
 
         Returns:
-            dict: A dictionary containing
-            the requested attributes of the student.
+            dict: A dictionary containing the requested attributes of the student.
         """
-        if (isinstance(attrs, list) and
-            all(isinstance(attr, str) for attr in attrs)):
-
-        return {
-            attr: getattr(self, attr)
-            for attr in attrs if hasattr(self, attr)
-        }
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs if hasattr(self, attr)
+            }
 
     def reload_from_json(self, json):
         """
-        Replaces all attributes of the Student instance
-        with values from a dictionary.
+        Replaces all attributes of the Student instance with values from a dictionary.
 
         Args:
-            json (dict): A dictionary where keys are 
-            attribute names and values are attribute values.
+            json (dict): A dictionary where keys are attribute names and values are attribute values.
         """
         for key, value in json.items():
             setattr(self, key, value)
