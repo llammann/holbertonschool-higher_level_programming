3aa48b7c3729b2ee65c6842079954f3a6c2701fb
llammann <bd7n2v9tr@code.edu.az>
2024-06-08 19:42:44 +0000

    Save Object to a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -14,15 +14,5 @@ def save_to_json_file(my_obj, filename):
         my_obj: The object to be saved to the file.
         filename: The file name.
     """
-    try:
-        json_string = json.dumps(my_obj)
-    except TypeError as e:
-        print(f"[TypeError] {e}")
-        return
-
-    try:
-        with open(filename, 'w') as file:
-            file.write(json_string)
-    except PermissionError as e:
-        print(f"[PermissionError] {e}")
-        print(f"[PermissionError] {filename}")
+    with open(filename, 'w') as file:
+        json.dumps(my_obj,filename)
