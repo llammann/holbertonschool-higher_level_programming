aa9e8be5328164563682781d841ca22363199b31
llammann <bd7n2v9tr@code.edu.az>
2024-06-08 16:03:49 +0000

    Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns the number of characters written.
+
+    Args:
+        filename (str): The file name
+        text (str): The text
+
+    Returns:
+        int: The number of characters.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
