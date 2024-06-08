b1f87ee17406501bdf1e7157bc2e890e5818fbf6
llammann <bd7n2v9tr@code.edu.az>
2024-06-08 23:19:20 +0000

    Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module defines a function `pascal_triangle`.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's triangle up to the given number of rows.
+
+    Args:
+        n (int): Number of rows in Pascal's triangle.
+
+    Returns:
+        list: List of lists representing Pascal's triangle.
+    """
+    triangle = []
+    if n <= 0:
+        return triangle
+
+    for i in range(n):
+        row = [None] * (i + 1)
+        row[0], row[-1] = 1, 1
+        for j in range(1, len(row) - 1):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
+
+def print_triangle(triangle):
+    """
+    Print Pascal's triangle.
+
+    Args:
+        triangle (list): List of lists representing Pascal's triangle.
+    """
+    for row in triangle:
+        print("[{}]".format(",".join(map(str, row))))
