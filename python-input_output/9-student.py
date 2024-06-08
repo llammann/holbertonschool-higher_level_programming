d82222fc02056b7b8990534278fea568290fba3b
llammann <bd7n2v9tr@code.edu.az>
2024-06-08 21:43:42 +0000

    Student to JSON

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -8,11 +8,10 @@ class Student:
     """
     Defines a student with first name, last name, and age.
     """
-
-
     def __init__(self, first_name, last_name, age):
         """
-        Initializes the student instance with the given first name, last name, and age.
+        Initializes the student instance with
+        the given first name, last name, and age.
 
         Args:
             first_name (str): The first name of the student.
@@ -22,13 +21,12 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
-
     def to_json(self):
         """
         Retrieves a dictionary representation of the student instance.
 
         Returns:
-            dict: A dictionary containing the student's first name, last name, and age.
+            dict: A dictionary containing
+            the student's first name, last name, and age.
         """
         return self.__dict__
