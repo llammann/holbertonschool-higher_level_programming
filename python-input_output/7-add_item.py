060161549d24935e82cdb70db4e1868257f89171
llammann <bd7n2v9tr@code.edu.az>
2024-06-08 18:23:14 +0000

    Load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+from os.path import exists
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+
+def add_items_to_list_and_save():
+    """
+    Adds command-line arguments to a list and saves them to a JSON file.
+    """
+    if exists("add_item.json"):
+        items = load_from_json_file("add_item.json")
+    else:
+        items = []
+
+    items.extend(sys.argv[1:])
+
+    save_to_json_file(items, "add_item.json")
+
+if __name__ == "__main__":
+    add_items_to_list_and_save()
