d426d5cdbb96c0954e404f481e3dd5ae1bb93516
llammann <bd7n2v9tr@code.edu.az>
2024-06-08 21:44:40 +0000

    Student to JSON

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -21,6 +21,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """
         Retrieves a dictionary representation of the student instance.
