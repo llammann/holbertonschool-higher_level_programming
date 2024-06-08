e7875129c0aa0053c83f028ac9f4c06767918f61
llammann <bd7n2v9tr@code.edu.az>
2024-06-08 20:43:17 +0000

    changed

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,10 +9,12 @@ from os.path import exists
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+
 def add_items_to_list_and_save():
     """
     Adds command-line arguments to a list and saves them to a JSON file.
     """
+    
     if exists("add_item.json"):
         items = load_from_json_file("add_item.json")
     else:
