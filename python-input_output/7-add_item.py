94f9883e4621159af72b4c816b2322b0ee81d470
llammann <bd7n2v9tr@code.edu.az>
2024-06-08 18:43:55 +0000

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,7 +5,8 @@ This module contains a function `add_items_to_list_and_save`.
 
 import sys
 from os.path import exists
-from json_functions import save_to_json_file, load_from_json_file
+load_from_json_file = __import__("6-load_from_json_file.py")
+save_to_json_file = __import__("5-save_to_json_file.py")
 
 
 def add_items_to_list_and_save():
