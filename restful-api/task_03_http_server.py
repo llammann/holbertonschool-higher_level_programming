7b573930f05111ea65ea49bde3cbce4f49913cf9
llammann <bd7n2v9tr@code.edu.az>
2024-06-13 16:25:27 +0000

    Develop a simple API using Python with the `http.server` module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -20,7 +20,7 @@ class APIServer(BaseHTTPRequestHandler):
             self.wfile.write("OK".encode('utf-8'))
         else:
             self._set_response(status_code=404, content_type='text/plain')
-            self.wfile.write("Not Found".encode('utf-8'))  # Ensure this matches the expected content
+            self.wfile.write("Endpoint not found".encode('utf-8'))
 
 def run(server_class=HTTPServer, handler_class=APIServer, port=8000):
     server_address = ('', port)
