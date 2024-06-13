7095a5b957ee0c38fad2ccf880e793c2f3367a02
llammann <bd7n2v9tr@code.edu.az>
2024-06-13 16:34:31 +0000

    Develop a simple API using Python with the `http.server` module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -26,7 +26,10 @@ def run(server_class=HTTPServer, handler_class=APIServer, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print(f"Starting server on port {port}...")
-    httpd.serve_forever()
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nServer stopped.")
 
 if __name__ == '__main__':
     run()
