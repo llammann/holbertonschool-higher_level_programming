1bca50aed17ea73b953a5044edebe314be2ed923
llammann <bd7n2v9tr@code.edu.az>
2024-06-30 17:16:34 +0000

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,13 @@
 -- Script to get the full description of first_table
 -- Author: [Laman]
 
-SELECT CONCAT('first_table', 'CREATE TABLE `first_table` (\n  `id` int DEFAULT NULL,\n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci') AS description;
-
+SELECT CONCAT(
+    'first_table',
+    'CREATE TABLE `first_table` (',
+    '`id` int NOT NULL AUTO_INCREMENT,',
+    '`name` varchar(128) DEFAULT NULL,',
+    '`c` char(1) DEFAULT NULL,',
+    '`created_at` date DEFAULT NULL,',
+    'PRIMARY KEY (`id`)',
+    ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
+) AS description;
