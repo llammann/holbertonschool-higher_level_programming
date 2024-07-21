436025608615853beb2d721ab3a3ce56070859b9
llammann <bd7n2v9tr.code.edu.az>
2024-07-21 19:43:19 +0400

    added

---
+++ python-server_side_rendering/read_json.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import json
+import csv
+import sqlite3
+
+def read(filename):
+     with open(filename, "r") as file:
+        data = json.load(file)
+        values = data.get("items", [])
+        return values
+     
+def read_json():
+    with open('products.json') as f:
+        products = json.load(f)
+    return products
+
+def read_csv():
+    products = []
+    with open('products.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            products.append({
+				"id": int(row['id']),
+				"name": row['name'],
+				"category": row['category'],
+				"price": float(row['price'])
+            })
+    return products       
+
+def read_db():
+    products = []
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Products")
+    product = cursor.fetchall()
+    conn.close()
+    for row in product:
+        products.append({
+            'id': row[0],
+            'name': row[1],
+            'category': row[2],
+            'price': row[3]
+        })
+    return products
