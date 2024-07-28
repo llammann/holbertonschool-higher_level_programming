15a2c93e499adc36d0a9b21165db66167bc55007
llammann <bd7n2v9tr.code.edu.az>
2024-07-28 22:34:22 +0400

    Star wars character

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://swapi-api.hbtn.io/api/people/5/?format=json")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("character").textContent = data.name;
+    })
+    .catch((error) => console.error("Error:", error));
+});
